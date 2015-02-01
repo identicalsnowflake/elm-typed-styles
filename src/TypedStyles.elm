bb01160e216b737959e87b22aa4b791ec028c0a0
identical snowflake <identicalsnowflake@yandex.com>
2015-02-01 05:05:12 +0300

    put documentation in module

--- src/TypedStyles.elm
+++ src/TypedStyles.elm
@@ -1,5 +1,34 @@
 module TypedStyles where
 
+{-|
+
+# Typed Styles for Elm
+
+Adds typed styles on top of elm-html. Example usage:
+
+```haskell
+import Html(..)
+import Html.Attributes(..)
+import TypedStyles(..)
+
+main : Html
+main = div [
+    style [
+        fontSize 30 px
+      , backgroundColor blue
+      , textCenter
+      , padding 20 px
+      , color white]
+      ]
+    [text "this is an example" ]
+
+```
+
+* I will add more stuff as I have time; in particular I want to try to get
+animation, transition, and transformation bindings.
+
+-}
+
 import Html(..)
 import Html.Attributes(..)
 import List(..)
